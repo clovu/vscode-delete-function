39dd6125b84672996abf01817577588223d1a419
Clover You <hi@clovu.me>
2026-06-07 07:51:04 +0800

    chore: remove builded products

--- rust-wasm/pkg/delete_function_vsc.d.ts
+++
@@ -1,8 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-/**
-* @param {string} code
-* @param {number} focus_line
-* @returns {string | undefined}
-*/
-export function rust2ast(code: string, focus_line: number): string | undefined;
