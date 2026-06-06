9c60d5b0a00116c7344f02649fe4a60d4a5b0f14
Clover You <hi@clovu.me>
2026-06-06 13:40:39 +0800

    test: enable tests for rust2ast function and fix formatting

--- src/tests/handleRs.spec.ts
+++ src/tests/handleRs.spec.ts
@@ -1,31 +1,31 @@
-// TODO: vitest 无法载入 wasm 模块: Error: [vite-node] Failed to load pkg/delete_function_vsc
-// import { rust2ast } from 'pkg/delete_function_vsc';
-// import { describe, test } from 'vitest';
+import { rust2ast } from 'pkg/delete_function_vsc';
+import { describe, test, expect } from 'vitest';
 
-// describe("handle rust", () => {
+describe("handle rust", () => {
 
-//   test("should delete function at focurs Position", () => {
-//     const code = `
-//       const TIPS: &[&str] = &[
-//         "Click on any AST node with a '+' to expand it",
+    test("should delete function at focurs Position", () => {
+        const code = `const TIPS: &[&str] = &[
+        "Click on any AST node with a '+' to expand it",
 
-//         "Hovering over a node highlights the \
-//         corresponding location in the source code",
+        "Hovering over a node highlights the \
+        corresponding location in the source code",
 
-//         "Shift click on an AST node to expand the whole subtree",
-//       ];
+        "Shift click on an AST node to expand the whole subtree",
+      ];
 
-//       pub fn print_tips() {
-//         for (i, tip) in TIPS.iter().enumerate() {
-//             println!("Tip {}: {}.", i, tip);
-//         }
-//       }
-//     `;
-//     // const range = rust2ast(code, 10);
-//     console.log(rust2ast);
+      pub fn print_tips() {
+        for (i, tip) in TIPS.iter().enumerate() {
+            println!("Tip {}: {}.", i, tip);
+        }
+      }
+    `;
+        const range = rust2ast(code, 10) ?? '{}';
 
+        expect(JSON.parse(range)).toEqual(
+            expect.objectContaining({
+                "start": { "line": 9, "column": 6 }, "end": { "line": 13, "column": 7 }
+            })
+        )
 
-//     // expect(range).toEqual([10, 14]);
-
-//   });
-// });
+    });
+});
