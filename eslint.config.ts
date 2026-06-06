2cac4466e080cb572cd338664b4922c4721c3ce7
Clover You <hi@clovu.me>
2026-06-06 14:00:44 +0800

    chore: update eslint 8 to 10 version

---
+++ eslint.config.ts
@@ -0,0 +1,19 @@
+import tseslint from 'typescript-eslint'
+import { defineConfig } from 'eslint/config'
+
+export default defineConfig(
+  { ignores: ['out', 'dist', '**/*.d.ts'] },
+  { extends: [tseslint.configs.recommended] },
+  {
+    files: ['src/**/*.ts'],
+    rules: {
+      '@typescript-eslint/naming-convention': 'warn',
+      'curly': 'warn',
+      'eqeqeq': 'warn',
+      'no-throw-literal': 'warn',
+      'semi': 'off',
+      'ts/explicit-function-return-type': 'off',
+      'markdown/require-alt-text': 'off',
+    },
+  },
+)
